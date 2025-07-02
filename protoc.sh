d2bb4fe114548472c4b4ab574c42908c02beb5aa
Nicholas Braun <tech@nicholasbraun.de>
2025-07-02 12:13:59 +0200

    asd

--- protoc.sh
+++ protoc.sh
@@ -9,7 +9,8 @@ git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
-sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+sudo apt update
+sudo apt install -y protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
